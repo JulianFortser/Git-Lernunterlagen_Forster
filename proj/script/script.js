a91942b28fde8bee235732b48c6d4d60dbc2df2c
Julian Forster <j.forster1@students.htl-leonding.ac.at>
2025-01-20 11:49:09 +0100

    Sliding Text animation generator function

--- proj/script/script.js
+++ proj/script/script.js
@@ -1,20 +1,20 @@
 let answersD = [
-    /*1*/ 
+    /*1*/
     /*2*/
     /*3*/
     /*4*/
 ]
 let questionsD = [
-    /*1*/   
+    /*1*/
     /*2*/
     /*3*/
     /*4*/
 ]
 let answersE = [
-    /*1*/ { A: "git init",B: "git start", C: "", D: ""},
-    /*2*/ { A: "",B: "", C: "", D: ""},
-    /*3*/ { A: "",B: "", C: "", D: ""},
-    /*4*/ { A: "",B: "", C: "", D: ""}
+    /*1*/ { A: "git init", B: "git start", C: "", D: "" },
+    /*2*/ { A: "", B: "", C: "", D: "" },
+    /*3*/ { A: "", B: "", C: "", D: "" },
+    /*4*/ { A: "", B: "", C: "", D: "" }
 ]
 let questionsE = [
     /*1*/ "What command would you use to create a new Git repository in your current directory?",
@@ -22,3 +22,27 @@ let questionsE = [
     /*3*/ "",
     /*4*/ ""
 ]
+
+function generateIndividualSlideFrames(text) {
+    const frames = [];
+    const length = text.length;
+    let currentFrame = "_".repeat(length).split("");
+
+    // Füge jeden Buchstaben einzeln hinzu und lasse ihn "sliden"
+    for (let i = 0; i < length; i++) {
+        for (let j = length - 1; j >= i; j--) {
+            currentFrame[j] = text[i];
+            frames.push(currentFrame.join(""));
+            currentFrame[j] = "_"; // Zurücksetzen für den nächsten Slide
+        }
+        currentFrame[i] = text[i]; // Buchstaben an der finalen Position fixieren
+    }
+
+    return frames;
+}
+
+// Beispielnutzung:
+const text = "Hallo";
+const frames = generateIndividualSlideFrames(text);
+
+console.log(frames);
